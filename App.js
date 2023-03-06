d39954ca1bf552a8f5d788a89fe7d55c179cc6c0
Maisa <maisa.folgueral@gmail.com>
2023-03-06 17:42:37 -0300

    Updated principal class

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 const DataBase = require("./Database");
 const Author = require("./entities/Author");
+const Order = require("./entities/Order");
 const User = require("./entities/User");
 
 module.exports = class App {
@@ -21,5 +22,61 @@ module.exports = class App {
 
   getAuthors() {
     App.#database.find("authors");
+  }
+
+  createBook(
+    title,
+    synopsis,
+    genre,
+    pages,
+    author,
+    description,
+    price,
+    inStock
+  ) {
+    const book = new Book(
+      title,
+      synopsis,
+      genre,
+      pages,
+      author,
+      description,
+      price,
+      inStock
+    );
+    App.#database.saveBook(book);
+  }
+
+  addBook(bookName, quantity) {
+    App.#database.addBookToStock(bookName, quantity);
+  }
+
+  createPoster(name, description, height, width, price, inStock) {
+    const poster = new Poster(name, description, height, width, price, inStock);
+    App.#database.savePoster(poster);
+  }
+
+  addPoster(posterName, quantity) {
+    App.#database.addPosterToStock(posterName, quantity);
+  }
+
+  createOrder(items, user) {
+    const order = new Order(items, user);
+    App.#database.saveOrder(order);
+    order.data.forEach(({ product, quantity }) => {
+      if (product instanceof Book) {
+        App.#database.removeBooksFromStock(product.name, quantity);
+      } else if (product instanceof Poster) {
+        App.#database.removePostersFromStock(product.name, quantity);
+      }
+    });
+  }
+
+  getOrders() {
+    return App.#database.find("orders");
+  }
+
+  showDatabase() {
+    App.#database.showStorage();
   }
 };
